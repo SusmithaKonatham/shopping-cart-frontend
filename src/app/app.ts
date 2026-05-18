1ee4e7f5befac1c5902874759ade3dbccdfec984
SusmithaKonatham <78208308+SusmithaKonatham@users.noreply.github.com>
2026-05-18 15:24:34 +0530

    scroller added and modified product page
    scroller added and modified product page

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Shopping Frontend');
+  protected readonly title = signal('Online Store');
 }
